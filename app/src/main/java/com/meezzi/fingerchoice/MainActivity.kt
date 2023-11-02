2ddd7682113f3a0ca7a76495e18ac192f044450f
Meezzi <alswl-3016@naver.com>
2023-11-02 09:55:56 +0900

    feat: NavController 활용

--- app/src/main/java/com/meezzi/fingerchoice/MainActivity.kt
+++ app/src/main/java/com/meezzi/fingerchoice/MainActivity.kt
@@ -2,6 +2,8 @@ package com.meezzi.fingerchoice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.meezzi.fingerchoice.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,5 +14,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val navController = supportFragmentManager.findFragmentById(R.id.home_container)?.findNavController()
+        navController?.let {
+            binding.bottomNavigation.setupWithNavController(it)
+        }
     }
 }
