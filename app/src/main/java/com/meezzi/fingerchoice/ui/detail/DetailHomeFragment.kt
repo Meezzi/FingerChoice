03d3e97cffca380a343fafdc001020f1a546a00c
Meezzi <alswl-3016@naver.com>
2023-11-27 15:29:15 +0900

    feat: 리뷰 작성 화면 이동

--- app/src/main/java/com/meezzi/fingerchoice/ui/detail/DetailHomeFragment.kt
+++ app/src/main/java/com/meezzi/fingerchoice/ui/detail/DetailHomeFragment.kt
@@ -21,7 +21,16 @@ class DetailHomeFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_detail_home, container, false)
+    ): View {
+        _binding = FragmentDetailHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.homeWriteReview.setOnClickListener {
+            findNavController().navigate(R.id.action_navigation_detail_restaurant_to_navigation_write_review)
+        }
     }
 }
