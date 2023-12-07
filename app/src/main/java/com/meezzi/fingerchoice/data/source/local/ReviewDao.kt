b94cd96a52780f2a9114cb3ee65722efeaee6d06
Meezzi <alswl-3016@naver.com>
2023-12-07 20:16:11 +0900

    feat: 리뷰 조회 Query 구현

--- app/src/main/java/com/meezzi/fingerchoice/data/source/local/ReviewDao.kt
+++ app/src/main/java/com/meezzi/fingerchoice/data/source/local/ReviewDao.kt
@@ -2,10 +2,14 @@ package com.meezzi.fingerchoice.data.source.local
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.Query
 
 @Dao
 interface ReviewDao {
 
     @Insert
     suspend fun insertReview(review: Review)
+
+    @Query("SELECT * FROM review")
+    suspend fun getAllReview(): List<Review>
 }
