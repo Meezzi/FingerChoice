71697fe407d05df1dc86551846c468100e452fa1
Meezzi <alswl-3016@naver.com>
2024-04-22 08:59:59 +0900

    build: 네이티브 앱 키 추가

--- app/src/main/java/com/meezzi/fingerchoice/FingerChoiceApplication.kt
+++ app/src/main/java/com/meezzi/fingerchoice/FingerChoiceApplication.kt
@@ -1,6 +1,7 @@
 package com.meezzi.fingerchoice
 
 import android.app.Application
+import com.kakao.vectormap.KakaoMapSdk
 import com.meezzi.fingerchoice.data.source.local.ReviewDatabase
 
 class FingerChoiceApplication : Application() {
@@ -8,6 +9,7 @@ class FingerChoiceApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         database = ReviewDatabase.getDatabase(this)
+        KakaoMapSdk.init(this, BuildConfig.KAKAO_MAP_KEY)
     }
 
     companion object {
