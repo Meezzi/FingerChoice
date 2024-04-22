f22c1a4ec305e0cee1f7f165ca72068f6431917f
Meezzi <alswl-3016@naver.com>
2024-04-22 10:59:06 +0900

    build: RestaurantRepository 추가

--- app/src/main/java/com/meezzi/fingerchoice/ui/composition/WriteReviewViewModel.kt
+++ app/src/main/java/com/meezzi/fingerchoice/ui/composition/WriteReviewViewModel.kt
@@ -5,11 +5,15 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
+import com.meezzi.fingerchoice.data.repository.RestaurantRepository
 import com.meezzi.fingerchoice.data.repository.ReviewRepository
 import com.meezzi.fingerchoice.data.source.local.Review
 import kotlinx.coroutines.launch
 
-class WriteReviewViewModel(private val reviewRepository: ReviewRepository) : ViewModel() {
+class WriteReviewViewModel(
+    private val reviewRepository: ReviewRepository,
+    private val restaurantRepository: RestaurantRepository
+) : ViewModel() {
 
     val score = MutableLiveData<Float>()
     val title = MutableLiveData<String>()
@@ -30,6 +34,37 @@ class WriteReviewViewModel(private val reviewRepository: ReviewRepository) : Vie
         val currentTitle = title.value ?: ""
         val currentContent = content.value ?: ""
         if (!isValidInfo(currentTitle) || !isValidInfo(currentContent)) return
+
+        if (isPrivateChecked.value != true) {
+            val reviews = com.meezzi.fingerchoice.data.model.Review(
+                currentTitle,
+                currentContent
+            )
+            Log.e("poiId", setPoiId.toString())
+            insertReviewToFirebaseDB(setPoiId.toString(), reviews)
+        } else insertReviewToLocalDB(listener, currentTitle, currentContent)
+    }
+
+    fun insertReviewToFirebaseDB(
+        poiId: String,
+        reviews: com.meezzi.fingerchoice.data.model.Review,
+    ) {
+        viewModelScope.launch {
+//            restaurantRepository.insertReview(
+//                name,
+//                poiId,
+//                reviews,
+//                score.value!!,
+//                location,
+//            )
+        }
+    }
+
+    fun insertReviewToLocalDB(
+        listener: ReviewSaveClickListener,
+        currentTitle: String,
+        currentContent: String
+    ) {
 
         val review = Review(
             taste = taste.value!!,
@@ -55,10 +90,11 @@ class WriteReviewViewModel(private val reviewRepository: ReviewRepository) : Vie
 
     companion object {
         fun provideFactory(
-            repository: ReviewRepository,
+            reviewRepository: ReviewRepository,
+            restaurantRepository: RestaurantRepository,
         ) = viewModelFactory {
             initializer {
-                WriteReviewViewModel(repository)
+                WriteReviewViewModel(reviewRepository, restaurantRepository)
             }
         }
     }
