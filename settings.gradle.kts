68ca515b0d9797796c18435f9c6ccf60e1b3c37e
Meezzi <alswl-3016@naver.com>
2024-05-05 08:40:57 +0900

    build: Naver Map 의존성 추가

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,7 +10,7 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
-        maven("https://devrepo.kakao.com/nexus/repository/kakaomap-releases/")
+        maven("https://repository.map.naver.com/archive/maven")
     }
 }
 
